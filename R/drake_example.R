4ec3d653d887a3b8a4b63bb12ef43a12d1e26481
Lorenzo Braschi <lorenzo.braschi@roche.com>
2019-03-27 16:42:34 +0100

    Tested examples of connection to shinyapps.io - it works- and added content to README

--- R/drake_example.R
+++ R/drake_example.R
@@ -3,18 +3,37 @@
 # Loading libraries --------------
 
 library(drake)
+library(bsts)
 library(data.table)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(purrr)
-library(tibble)
+library(tsibble)
 library(padr)
 library(lubridate)
+library(ggridges)
+library(glue)
 
 # Custom functions --------------
 
+bsts_modelling <- function(data, niter = 1000, burn = .1){
+  ss <- AddSemilocalLinearTrend(list(), data)
+  ss <- AddAutoAr(ss, data$sales, lags = 1)
+  ss <- AddAutoAr(ss, data$sales, lags = 12)
+  ss <- AddSeasonal(ss, data, nseasons = 12)
 
+  model <- bsts(data, ss, niter = niter, ping = 0, model.options = BstsOptions(save.full.state = F))
+
+  burn <- SuggestBurn(burn, model)
+
+  model$mean.trend.contributions <- colMeans(model$state.contributions[-(1:burn),"trend",])
+  model$mean.season.contributions <- colMeans(model$state.contributions[-(1:burn),"seasonal.12.1",])
+  model$state.contributions <- model$state.contributions[niter,,]
+  model$one.step.prediction.errors <- model$one.step.prediction.errors[-(1:burn),]
+
+  return(model)
+}
 
 # Check (not get!) the files -----------------
 
@@ -26,16 +45,48 @@ file.exists("data/stations.csv")
 plan <- drake_plan(
   raw_data = fread(file_in("data/madrid_daily_pollution.csv")),
   stations = fread(file_in("data/stations.csv")),
-  nested = raw_data %>%
-    group_by(station) %>%
+  cleaned_data = raw_data %>%
+    mutate(date = ymd(date)) %>%
+    gather(key = "agent",
+           value = "conc",
+           -c(station, date)) %>%
+    as_tsibble(key = id(station, agent), index = date, regular = FALSE) %>%
+    pad(interval = "day") %>%
+    inner_join(stations, by = c("station" = "id")) %>%
+    select(station, name, address, lon, lat, elevation, everything()),
+  nested_station = cleaned_data %>%
+    group_by(station, name, address, lon, lat, elevation) %>%
     nest(),
-  test_plot = nested %>%
-    pluck("data", 2) %>%
-    ggplot(aes(date_thick, NO_2)) +
-    geom_line()
+  nested_agent = cleaned_data %>%
+    group_by(agent) %>%
+    nest(),
+  plot_station = nested_station %>%
+    mutate(plot = map2(
+      data,
+      name,
+      ~.x %>%
+        ggplot(aes(date, conc, color = agent)) +
+        geom_line() +
+        theme_minimal()+
+        labs(title = glue("Station measurements for station {station}"))+
+        facet_wrap(~agent, scales = "free_y", ncol = 3))
+    ),
+  plot_agent_ridge = nested_agent %>%
+    mutate(plot = map2(
+      data,
+      agent,
+      ~.x %>%
+        filter(!is.na(conc)) %>%
+        mutate(month = month(date, label = TRUE)) %>%
+        ggplot(aes(conc, as.factor(name), fill = as.factor(name)))+
+        geom_density_ridges(scale = 10, size = .25, show.legend = F, alpha = .8) +
+        scale_fill_viridis_d()+
+        theme_ridges(font_family = "Helvetica", font_size = 12)+
+        facet_wrap(~month, ncol = 4)+
+        labs(title = glue("Concentrations of {.y} per station")))
 
+    )
 )
-
 
 
 # Config ---------------
@@ -48,7 +99,7 @@ vis_drake_graph(config)
 
 # Run the plan ------------------
 
-make(plan, jobs = 2, parallelism = "future")
-progress()
+make(plan, jobs = 4, parallelism = "future")
 
-vis_drake_graph(config)
+readd(plot_agent_ridge) %>% filter(agent == "SO_2") %>% pluck("plot",1)
+
--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# c4l\_workshop
+# code4life workshop
 
 This is the repository for the code4life workshop held in Warsaw.
+
+# Requirements
+
+  - A github account
+  - R and RStudio
+  - Shinyapps.io account
+
+## R and RStudio
+
+In this workshop we will use R and RStudio, therefore they need to be
+installed in your local machines.
+
+You can get R [here](https://cran.r-project.org/) and RStudio
+[here](https://www.rstudio.com/products/rstudio/download/#download).
+
+## GitHub account
+
+Every participant should have a GitHub account ready for the workshop.
+Setting one up is easy and straightforward. Just go to
+[github.com](github.com) and follow the steps.
+
+## Shinyapps.io
+
+We will deploy our shiny application on [shinyapps.io](shinyapps.io),
+for which a free account is required.
