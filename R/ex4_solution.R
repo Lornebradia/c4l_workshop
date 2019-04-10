9b1d2798ae495cd41febeb5282d5977e7cb11310
Lorenzo Braschi Diaferia <Lornebradia@users.noreply.github.com>
2019-04-10 22:53:44 +0200

    Changes to readme (#3)
    * Full setup on README, missing github setup only
    
    * Resized images
    
    * Added description of functional programming with purrr
    
    * Links on images
    
    * Removed shiny app
    
    * Clarified that one needs to log in to rsconnect
    
    * Exercises solution (#2)
    
    * RDS files to .gitignore because only the code is real
    
    * RDS files to .gitignore because only the code is real
    
    * Report completed, ex5 solution created and completed, ex4 completed
    
    * Report completed, ex5 solution created and completed, ex4 completed
    
    * Report completed, ex5 solution created and completed, ex4 completed
    
    * Added some text to the flexdash, radio buttons for the seasonplot
    
    * Added more pollutants to the final calculations
    
    * Ex5 is now Ex6
    
    * Ex4 is simply transforming data and creating plots
    
    * testing html output
    
    * Removed html content, cannot be added
    
    * Formatted code for better view

--- R/ex4_solution.R
+++ R/ex4_solution.R
@@ -2,79 +2,44 @@
 #'
 #' We take the results from ex3
 
-#' Required libraries -------------------
+#' Setup -------------------
 
-library(forecast)
 theme_set(theme_minimal())
 
 #' Add to the plan --------------------------
 
 ex4_plan <- drake_plan(
-  # Yes, this is not optimal - we should go and fix the plan!
-  unnested = nested %>%
-    unnest(),
-  aggregated_renested = unnested %>%
+  aggregated_renested = filtered %>%
     group_by(agent, date) %>%
     summarise(concentration = mean(concentration, na.rm = TRUE)) %>%
     group_by(agent) %>%
     nest(.key = "history"),
   to_ts = aggregated_renested %>%
-    mutate(history = map(history,
-                         ~.x %>%
-                           rename(time = date) %>%
-                           tsbox::ts_ts())),
-  seasonal_plots = to_ts %>%
-    mutate(seasonal = map2(
+    mutate(history = map(
       history,
-      agent,
-      ~.x %>% ggseasonplot(year.labels = F) +
-        stat_smooth(aes(group = NULL, color = NULL),
-                    se = FALSE, method = "loess") +
-        scale_x_continuous(breaks = seq(0, 1, length.out = 12),
-                           labels = month.abb) +
-        labs(title = glue::glue("Seasonal plot for {.y}")))),
-  # We will train three models in one
-  modelling = target(
-    to_ts %>% mutate(model = map(history, ~.x %>% stlf(method = how))),
-    transform = map(how = c("ets", "arima", "naive"))
-    ),
-  combined = target(
-    bind_rows(list(ets = modelling_.ets.,
-                   arima = modelling_.arima.,
-                   naive = modelling_.naive.),
-              .id = "m_function")),
-  plotting = combined %>%
+      ~ .x %>%
+        rename(time = date) %>%
+        tsbox::ts_ts()
+    )),
+  seasonal_plots = to_ts %>%
     mutate(
-      accuracy = map(model,
-                     ~.x %>%
-                       accuracy() %>%
-                       as_tibble(rownames = "set")),
-      fplot = pmap(
-        list(model,
-             agent,
-             m_function
-             ),
-        function(x, y, z){
-
-          x %>% autoplot() +
-            labs(x = "Year",
-                 y = "Concentration",
-                 subtitle = glue::glue("Polluting agent: {y}"),
-                 caption = glue::glue("Modelling function: {z}")) +
-            annotate("label", hjust = 0,
-                     x = 1,
-                     y = 0,
-                     label = glue::glue("MAPE 1000 %"))
-        }
-
+      seasonal = map2(
+        history,
+        agent,
+        ~ .x %>%
+          ggseasonplot(year.labels = F) +
+          stat_smooth(aes(group = NULL, color = NULL),
+                      se = FALSE, method = "loess") +
+          scale_x_continuous(breaks = seq(0, 1, length.out = 12),
+                             labels = month.abb) +
+          labs(title = glue::glue("Seasonal plot for {.y}"))
       )
     )
-
 )
 
 # Add to the previous plan
 
-ex4_plan <- bind_rows(ex3_plan, ex4_plan)
+ex4_plan <- bind_plans(ex3_plan, ex4_plan)
 
 # Config --------------------
 
@@ -86,6 +51,4 @@ vis_drake_graph(ex4_conf)
 make(ex4_plan)
 
 vis_drake_graph(ex4_conf, collapse = TRUE)
-readd(plotting) %>% pluck("fplot",5)
-
-
+readd(final) %>% pluck("fplot", 5)
